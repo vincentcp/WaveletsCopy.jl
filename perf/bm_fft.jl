40251be54888fbb0916d2af64b173628ed52da24
vincentcp <vincent.coppe@gmail.com>
2017-07-13 16:06:04 +0200

    Clean up of old files

--- perf/bm_fft.jl
+++
@@ -1,8 +0,0 @@
-
-include("setup_1d.jl")
-
-println("fft (N=",N,"), (FFTW)")
-f(x0) = for i = 1:tn; fft(x0); end
-f(x0);
-@time f(x0);
-
--- perf/bm_idwt_filt.jl
+++
@@ -1,8 +0,0 @@
-
-include("setup_1d.jl")
-
-println("idwt by filtering (N=",N,"), ", L, " levels")
-f(x0, L, wf) = for i = 1:tn; idwt(x0, wf, L); end
-f(x0, L, wf);
-@time f(x0, L, wf);
-
