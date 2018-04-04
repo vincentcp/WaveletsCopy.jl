de91997abb87f87bdb91fb74e6561c211a3157a2
vincentcp <vincent.coppe@gmail.com>
2018-04-04 16:20:30 +0200

    add comment to scaling coefficients

--- src/dwt/dwttransform.jl
+++ src/dwt/dwttransform.jl
@@ -2,11 +2,11 @@
 using ..Util: isdyadic, maxtransformlevels, ndyadicscales
 
 export dwt, idwt, full_dwt, full_idwt
-"Perform l steps of the wavelet transform"
+"Perform `L` steps of the wavelet transform"
 function dwt!
 end
 
-"Perform l steps of the inverse wavelet transform"
+"Perform `L` steps of the inverse wavelet transform"
 function idwt!
 end
 
@@ -20,74 +20,52 @@ idwt(x, w::DiscreteWavelet, bnd::WaveletBoundary, L::Int=maxtransformlevels(x)) 
     idwt(x, Filterbank(w), bnd, L)
 
 function dwt!(y, x, fb::Filterbank, bnd::WaveletBoundary, L::Int=maxtransformlevels(x))
-  lx = length(x)
-  @assert 2^L <= lx
-  copy!(y,x)
-  for l in 1:L
-    lx2 = lx >> 1
-    y[1:lx2], y[lx2+1:lx] = DWT.dwtstep(y[1:lx], fb, bnd)
-    lx = lx2
-  end
+    lx = length(x)
+    @assert 2^L <= lx
+    copy!(y,x)
+    for l in 1:L
+        lx2 = lx >> 1
+        y[1:lx2], y[lx2+1:lx] = DWT.dwtstep(y[1:lx], fb, bnd)
+        lx = lx2
+    end
 end
 
 function dwt(x, fb::Filterbank, bnd::WaveletBoundary,  L::Int=maxtransformlevels(x))
-  @assert isdyadic(x)
-  T = promote_type(eltype(x), eltype(fb))
-  y = zeros(T, dwt_size(x, fb, bnd))
-  dwt!(y, x, fb, bnd, L)
-  y
+    @assert isdyadic(x)
+    T = promote_type(eltype(x), eltype(fb))
+    y = zeros(T, dwt_size(x, fb, bnd))
+    dwt!(y, x, fb, bnd, L)
+    y
 end
 
 function idwt!(y, x, fb::Filterbank, bnd::WaveletBoundary, L::Int=maxtransformlevels(x))
-  lx = length(x)
-  @assert 2^L <= lx
-  copy!(y,x)
-  lx = lx >> L
-  for l in L:-1:1
-    lx2 = lx << 1
-    t = DWT.idwtstep(y[1:lx], y[lx+1:lx2], fb, bnd)
-    y[1:lx2] = t
-    lx = lx2
-  end
+    lx = length(x)
+    @assert 2^L <= lx
+    copy!(y,x)
+    lx = lx >> L
+    for l in L:-1:1
+        lx2 = lx << 1
+        t = DWT.idwtstep(y[1:lx], y[lx+1:lx2], fb, bnd)
+        y[1:lx2] = t
+        lx = lx2
+    end
 end
 
 function idwt(x, fb::Filterbank, bnd::WaveletBoundary, L::Int=maxtransformlevels(x))
-  @assert isdyadic(x)
-  T = promote_type(eltype(x), eltype(fb))
-  y = zeros(T, dwt_size(x, fb, bnd))
-  idwt!(y, x, fb, bnd, L)
-  y
+    @assert isdyadic(x)
+    T = promote_type(eltype(x), eltype(fb))
+    y = zeros(T, dwt_size(x, fb, bnd))
+    idwt!(y, x, fb, bnd, L)
+    y
 end
 
-# function full_dwt!(y, x, w::DiscreteWavelet, bnd::WaveletBoundary, scratch = similar(y))
-#   scaling_coefficients!(scratch, x, w, bnd)
-#   dwt!(y, scratch, Filterbank(w), bnd)
-# end
-
-# function full_idwt!(y, x, w::DiscreteWavelet, bnd::WaveletBoundary, scalingcoefs=nothing, scratch=nothing, scratch2=nothing)
-#   idwt!(scalingcoefs, x, Filterbank(w), bnd)
-#   scaling_coefficients_to_dyadic_grid!(y, scalingcoefs, w, bnd, scratch, scratch2)
-# end
-
+""
 function full_dwt{T}(x, w::DiscreteWavelet{T}, bnd::WaveletBoundary)
-  coefs = scaling_coefficients(x, w, bnd)
-  dwt(coefs, Filterbank(w), bnd)
-  # ELT = eltype(T, eltype(x))
-  # y = zeros(ELT, dwt_size(x, w, bnd))
-  # scratch = zeros(ELT, dwt_size(x, w, bnd))
-  # full_dwt!(y, x, w, bnd, scratch)
-  # y
+    coefs = scaling_coefficients(x, w, bnd)
+    dwt(coefs, Filterbank(w), bnd)
 end
 
 function full_idwt{T}(x, w::DiscreteWavelet{T}, bnd::WaveletBoundary)
-  scalingcoefs = idwt(x, Filterbank(w), bnd)
-  scaling_coefficients_to_dyadic_grid(scalingcoefs, w, bnd)
-  # ELT = eltype(T, eltype(x))
-  # y = zeros(ELT, dwt_size(x, w, bnd))
-  # scalingcoefs = zeros(ELT, dwt_size(x, w, bnd))
-  # d = ndyadicscales(scalingcoefs)
-  # scratch = zeros(DWT.scaling_coefficients_to_dyadic_grid_scratch_length(w,d))
-  # scratch2 = zeros(DWT.scaling_coefficients_to_dyadic_grid_scratch2_length(w,d))
-  # full_idwt!(y, x, w, bnd, scalingcoefs, scratch, scratch2)
-  # y
+    scalingcoefs = idwt(x, Filterbank(w), bnd)
+    scaling_coefficients_to_dyadic_grid(scalingcoefs, w, bnd)
 end
--- src/dwt/scaling_coefficients.jl
+++ src/dwt/scaling_coefficients.jl
@@ -1,82 +1,127 @@
 # scaling_coefficients.jl
 import ..Util: ndyadicscales, isdyadic
+"""
+    The scaling coeffients function deals with the wavelt crime.
+    The input of the DWT are inner products between dual functions and the function `f`
+    ```
+    v_Nk = <ϕ̃_Nk, f>
+    ```
+    or also the scaling coefficients of the projection of `f`, `P_Nf`, on `V_N`
+    ```
+    P_Nf(t) = \sum_k v_Nkϕ_Nk(t)
+    ```
+    not function samples
+    ```
+    f_Nk = f(kΔt), Δt = 2^{-N}, N = 2^L
+    ```
+    Replacing `v_Nk` with `f_k` is the wavelet crime.
 
+    However, assuming that `L` is large and hence that `ϕ` and `ϕ̃` have a narrow support we can think of the
+    `v_Nk`s as scaled function values `f_k`
+    ```
+    v_Nk ≈\sqrt{Δt/θ}f_Nk, θ=\sqrt{2π}
+    ```
+
+    Then
+    ```
+    P_Nf(t) = 1/θ\sum_l f(t-lΔt)ϕ(l) + O(Δt)
+    ```
+    but convergence is slow...
+
+    Therefore, we use approximants ṽ_Nk for v_Nk using the Riemann sum
+
+    ```
+    ṽ_Nk = 1/\sqrt{2π} \sum_n f_n2^{N/2}\overline{ϕ̃(2^NnΔt-k)}Δt
+         = 2^{-N/2}/\sqrt{2π}\sum_n f_n\overline{ϕ̃(n-k)}
+    ```
+    which is a convolution between function values and \{\overline{ϕ̃(-n)}\}
+    which is in turn a low pass filtering.
+"""
+function scaling_coefficients end
+
+
+# Function on the interval [a,b] to function on [0,1]
 function scaling_coefficients(f::Function, w::DWT.DiscreteWavelet, L::Int, fembedding, a::Real=0, b::Real=1; side::Side=Dul(), options...)
-  T = promote_type(eltype(w), eltype(a), eltype(b))
-  a = T(a); b = T(b)
-  flt = filter(side, Scl(), w)
-  (b-a)*scaling_coefficients(x->f((b-a)*x+a), flt, L::Int, fembedding; options...)
+      T = promote_type(eltype(w), eltype(a), eltype(b))
+      a = T(a); b = T(b)
+      flt = filter(side, Scl(), w)
+      (b-a)*scaling_coefficients(x->f((b-a)*x+a), flt, L::Int, fembedding; options...)
 end
 
-# Function on the interval (0 1) to scaling coefficients
+# Function on the interval [a,b] to samples
 function scaling_coefficients{T}(f::Function, s::CompactSequence{T}, L::Int, fembedding; options...)
-  x = linspace(T(0), T(1), 1<<L + 1)[1:end-1]
-  fcoefs = map(f, x)
-  @assert eltype(fcoefs)==T
-  filter = _scalingcoefficient_filter(s)
-  fembedding == nothing && (fembedding = FunctionEmbedding(k -> f(k*T(2)^(-T(L)))))
-  scaling_coefficients(fcoefs, filter, fembedding; options...)
+      x = linspace(T(0), T(1), 1<<L + 1)[1:end-1]
+      fcoefs = map(f, x)
+      @assert eltype(fcoefs)==T
+      filter = _scalingcoefficient_filter(s)
+      fembedding == nothing && (fembedding = FunctionEmbedding(k -> f(k*T(2)^(-T(L)))))
+      scaling_coefficients(fcoefs, filter, fembedding; options...)
 end
 
+# Convenience function: wavelet to filter
 scaling_coefficients{T}(f::AbstractArray, w::DiscreteWavelet{T}, bnd::PeriodicBoundary; options...) =
     scaling_coefficients(f, _scalingcoefficient_filter(filter(Dul(), Scl(), w)), PeriodicEmbedding(); options...)
 
+# Convenience function: wavelet to filter
 scaling_coefficients!{T}(c::AbstractArray, f::AbstractArray, w::DiscreteWavelet{T}, bnd::PeriodicBoundary; options...) =
     scaling_coefficients!(c, f, _scalingcoefficient_filter(filter(Dul(), Scl(), w)), PeriodicEmbedding(); options...)
 
-# Function evaluations on a dyadic grid to scaling coefficients
+
+# function samples to scaling coeffients
 function scaling_coefficients{T}(f::AbstractArray, filter::CompactSequence{T}, fembedding; n::Int=length(f), options...)
-  @assert isdyadic(f)
-  c = Array{T}(n)
-  scaling_coefficients!(c, f, filter, fembedding; options...)
-  c
+      @assert isdyadic(f)
+      c = Array{T}(n)
+      scaling_coefficients!(c, f, filter, fembedding; options...)
+      c
 end
 
+"In place method of scaling_coefficients"
 function scaling_coefficients!{T}(c, f, filter::CompactSequence{T}, fembedding; offset::Int=0, options...)
-  # TODO write a convolution function
-  # convolution between low pass filter and function values gives approximation of scaling coefficients
-  for j in offset:offset+length(c)-1
-    ci = zero(T)
-    for l in firstindex(filter):lastindex(filter)
-      ci += filter[l]*fembedding[f, j-l]
+    # TODO write a convolution function
+    # convolution between low pass filter and function values gives approximation of scaling coefficients
+    for j in offset:offset+length(c)-1
+        ci = zero(T)
+        for l in firstindex(filter):lastindex(filter)
+            ci += filter[l]*fembedding[f, j-l]
+        end
+        c[j+1-offset] = T(1)/T(sqrt(length(f)))*ci
     end
-    c[j+1-offset] = T(1)/T(sqrt(length(f)))*ci
-  end
 end
 
 _scalingcoefficient_filter(f::CompactSequence) =
     reverse(CompactSequence(cascade_algorithm(f, 0), f.offset))
 
-
+"Transforms scaling coeffients back to function evaluations on the dyadic grid."
 function scaling_coefficients_to_dyadic_grid{T}(scaling_coefficients::AbstractArray, w::DWT.DiscreteWavelet{T}, bnd::WaveletBoundary, d=ndyadicscales(scaling_coefficients); grid=false, options...)
-  function_evals = zeros(T,DWT.scaling_coefficients_to_dyadic_grid_length(w,d))
-  scratch = zeros(DWT.scaling_coefficients_to_dyadic_grid_scratch_length(w,d))
-  scratch2 = zeros(DWT.scaling_coefficients_to_dyadic_grid_scratch2_length(w,d))
+    function_evals = zeros(T,DWT.scaling_coefficients_to_dyadic_grid_length(w,d))
+    scratch = zeros(DWT.scaling_coefficients_to_dyadic_grid_scratch_length(w,d))
+    scratch2 = zeros(DWT.scaling_coefficients_to_dyadic_grid_scratch2_length(w,d))
 
-  scaling_coefficients_to_dyadic_grid!(function_evals, scaling_coefficients, w, bnd, scratch, scratch2; options...)
-  grid ?
-    (return function_evals, linspace(T(0), T(1), length(function_evals)+1)[1:end-1]) :
-    (return function_evals)
+    scaling_coefficients_to_dyadic_grid!(function_evals, scaling_coefficients, w, bnd, scratch, scratch2; options...)
+    grid ?
+        (return function_evals, linspace(T(0), T(1), length(function_evals)+1)[1:end-1]) :
+        (return function_evals)
 end
 
 # Scaling coefficients to function evaluations on dyadic grid (assumes periodic extension)
+"In place method of scaling_coefficients_to_dyadic_grid."
 function scaling_coefficients_to_dyadic_grid!{T}(function_evals::AbstractArray, scaling_coefficients::AbstractArray, w::DWT.DiscreteWavelet{T}, ::DWT.PeriodicBoundary, scratch=nothing, scratch2=nothing; grid=false, options...)
-  d = Int(log2(length(function_evals)))
-  @assert length(function_evals) == length(scratch)
-  @assert DWT.scaling_coefficients_to_dyadic_grid_scratch2_length(w, d) == length(scratch2)
-  @assert isdyadic(scaling_coefficients)
-  j = ndyadicscales(scaling_coefficients)
-  function_evals[:] = T(0)
-  for (c_i,c) in enumerate(scaling_coefficients)
-    k = c_i - 1
-    DWT.evaluate_periodic_in_dyadic_points!(scratch, DWT.Prl(), DWT.Scl(), w, j, k, d, scratch2, nothing)
-    scale!(scratch, c)
-    for i in 1:length(function_evals)
-      function_evals[i] += scratch[i]
+    d = Int(log2(length(function_evals)))
+    @assert length(function_evals) == length(scratch)
+    @assert DWT.scaling_coefficients_to_dyadic_grid_scratch2_length(w, d) == length(scratch2)
+    @assert isdyadic(scaling_coefficients)
+    j = ndyadicscales(scaling_coefficients)
+    function_evals[:] = T(0)
+    for (c_i,c) in enumerate(scaling_coefficients)
+        k = c_i - 1
+        DWT.evaluate_periodic_in_dyadic_points!(scratch, DWT.Prl(), DWT.Scl(), w, j, k, d, scratch2, nothing)
+        scale!(scratch, c)
+        for i in 1:length(function_evals)
+            function_evals[i] += scratch[i]
+        end
+        # function_evals[:] += c*DWT.evaluate_periodic_in_dyadic_points(Prl(), Scl(), w, j, k, d)
     end
-    # function_evals[:] += c*DWT.evaluate_periodic_in_dyadic_points(Prl(), Scl(), w, j, k, d)
-  end
-  nothing
+    nothing
 end
 
 scaling_coefficients_to_dyadic_grid_length(w::DWT.DiscreteWavelet, d::Int) = 1<<d
@@ -87,8 +132,8 @@ scaling_coefficients_to_dyadic_grid_scratch2_length(w::DWT.DiscreteWavelet, d::I
     DWT.evaluate_periodic_in_dyadic_points_scratch_length(DWT.Prl(), DWT.Scl(), w, d, 0, d)
 
 function DWT.support(side::Side, n::Int, i::Int, l::Int, w::DiscreteWavelet)
-  kind, j, k = wavelet_index(n,i,l)
-  support(side, kind, w, j, k)
+    kind, j, k = wavelet_index(n,i,l)
+    support(side, kind, w, j, k)
 end
 
 """
@@ -103,23 +148,28 @@ end
     (Scl(), 0, 0),    (Wvl(), 0, 0),    (Wvl(), 1, 0),     (Wvl(), 1, 1)
 """
 function wavelet_index(n::Int, i::Int, l::Int)
-  if i > n/(1<<l)
-    j = level(n,i)
-    k = mod(i-1,1<<j)
-    Wvl(), j, k
-  else
-    Scl(), Int(log2(n))-l, i-1
-  end
+    if i > n/(1<<l)
+        j = level(n,i)
+        k = mod(i-1,1<<j)
+        Wvl(), j, k
+    else
+        Scl(), Int(log2(n))-l, i-1
+    end
 end
 
+"""
+  The coefficient corresponding to the wavelet index (kind, j, k). The integer
+  is the index in the array of the DWT.
+"""
 coefficient_index(kind::Scl, j::Int, k::Int)::Int = k+1
 coefficient_index(kind::Wvl, j::Int, k::Int)::Int = 1<<j+k+1
 
+"Return the wavelet level of a coefficient in an array of length `n` with index `i`"
 function level(n::Int, i::Int)
-  (i == 1 || i == 2) && (return 0)
-  for l in 1:round(Int,log2(n))
-    if i <= (1<<(l+1))
-      return l
+    (i == 1 || i == 2) && (return 0)
+    for l in 1:round(Int,log2(n))
+        if i <= (1<<(l+1))
+            return l
+        end
     end
-  end
 end
